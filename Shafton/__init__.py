1920849eb07eaec05e3bb7748af80f35f333d397
iaggocapitanio1 <49845024+iaggocapitanio1@users.noreply.github.com>
2020-05-02 17:29:38 -0300

    Add files via upload

---
+++ Shafton/__init__.py
@@ -0,0 +1,10 @@
+
+"""
+@Description:
+This package'll help to project a mechanical shaft.
+
+"""
+__author__ = "Iaggo Capitanio"
+__copyright__ = "Copyright (C) 2020 Iaggo Capitanio"
+__license__ = "Public Domain"
+__version__ = "1.0"
